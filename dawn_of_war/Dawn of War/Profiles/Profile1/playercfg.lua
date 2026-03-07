1a7ecc499e9ae1faa654e315ca5ae154578f25fd
William Lumme <80401792+wlumme@users.noreply.github.com>
2026-03-07 23:17:09 +0000

    feat: Add Dawn of War files

---
+++ dawn_of_war/Dawn of War/Profiles/Profile1/playercfg.lua
@@ -0,0 +1,116 @@
+Controls =  
+{
+	cursorScale = 1,
+	enableChat = false,
+	enableCustomBadges = true,
+	hotkey_preset = "keydefaults",
+	invertDeclination = 0,
+	invertPan = 1,
+	lockCursor = true,
+	scrollRate = 1,
+	showHotkeys = true,
+}
+DoWDE_game_preferences =  
+{
+	num_teams = 2,
+	options =  
+	{
+		option0 =  
+		{
+			choice = "0",
+			id = "1095320646",
+		},
+		option1 =  
+		{
+			choice = "0",
+			id = "1381192532",
+		},
+		option2 =  
+		{
+			choice = "0",
+			id = "1280005197",
+		},
+		option3 =  
+		{
+			choice = "1",
+			id = "1128809793",
+		},
+		option4 =  
+		{
+			choice = "0",
+			id = "1397509955",
+		},
+		option5 =  
+		{
+			choice = "2",
+			id = "1196642372",
+		},
+		option6 =  
+		{
+			choice = "0",
+			id = "1381192520",
+		},
+		option7 =  
+		{
+			choice = "1",
+			id = "1381192276",
+		},
+		option8 =  
+		{
+			choice = "0",
+			id = "1313229913",
+		},
+	},
+	team_type = 0,
+	win_conditions =  
+	{
+		win_condition1 =  
+		{
+			id = "1003066394",
+		},
+		win_condition2 =  
+		{
+			id = "735076042",
+		},
+		win_condition3 =  
+		{
+			id = "1959084950",
+		},
+		win_condition4 =  
+		{
+			id = "69421273",
+		},
+	},
+}
+Sound =  
+{
+	VolumeAmbient = 0.75000,
+	VolumeMusic = 0.75000,
+	VolumeSfx = 0.75000,
+	VolumeVoice = 0.75000,
+}
+Tutorial_DoWDE =  
+{
+	HasClickedSkirmish = true,
+	HasPlayedTutorial = false,
+}
+W40k =  
+{
+	attack_move_default = 0,
+	event_cue_toggle_critical = 1,
+	event_cue_toggle_non_critical = 1,
+	help_text_level = 2,
+	persistent_selection = 1,
+	response_sound = 1,
+}
+ai_preferences1 =  
+{
+}
+player_preferences =  
+{
+	campaign_played_disorder = false,
+	campaign_played_order = false,
+	force_name = "Bwonzgob's Boyz",
+	race = "ork_race",
+}
+
